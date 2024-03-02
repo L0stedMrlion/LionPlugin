f551fab108484f04ed47f788a05dc72961821239
Mrlion <mrlion4202@gmail.com>
2024-03-02 11:02:40 +0100

    Add files via upload

---
+++ java/me/mrlion/lionplugin/commands/FlyCommand.java
@@ -0,0 +1,40 @@
+package me.mrlion.lionplugin.commands;
+
+import me.mrlion.lionplugin.LionPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FlyCommand implements CommandExecutor {
+
+    private final LionPlugin plugin;
+
+    public FlyCommand(LionPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(plugin.getConfig().getString("fly.player-only"));
+            return true;
+        }
+
+        Player player = (Player) sender;
+        if (!player.hasPermission("lionplugin.fly")) {
+            sender.sendMessage(plugin.getConfig().getString("fly.no-permission-fly"));
+            return true;
+        }
+
+        if (player.getAllowFlight()) {
+            player.setAllowFlight(false);
+            plugin.getLogger().info(player.getName() + " has disabled flight mode.");
+        } else {
+            player.setAllowFlight(true);
+            plugin.getLogger().info(player.getName() + " has enabled flight mode.");
+        }
+
+        return true;
+    }
+}
