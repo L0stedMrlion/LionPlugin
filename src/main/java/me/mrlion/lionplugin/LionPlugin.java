d468a86a578a3c45ea27bc038fbe26553d64d1bf
Mrlion <mrlion4202@gmail.com>
2024-03-04 14:13:43 +0100

    for command added new error handler

--- src/main/java/me/mrlion/lionplugin/LionPlugin.java
+++ src/main/java/me/mrlion/lionplugin/LionPlugin.java
@@ -2,6 +2,7 @@ package me.mrlion.lionplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
@@ -12,13 +13,13 @@ public class LionPlugin extends JavaPlugin {
 
         vanishedPlayers = new HashSet<>();
 
-        getCommand("fly").setExecutor(new me.mrlion.lionplugin.commands.FlyCommand(this));
-        getCommand("heal").setExecutor(new me.mrlion.lionplugin.commands.HealCommand(this));
-        getCommand("msg").setExecutor(new me.mrlion.lionplugin.commands.MsgCommand(this));
-        getCommand("lreload").setExecutor(new me.mrlion.lionplugin.commands.ReloadCommand(this));
-        getCommand("lwhitelist").setExecutor(new me.mrlion.lionplugin.commands.WhitelistCommand(this));
-        getCommand("ping").setExecutor(new me.mrlion.lionplugin.commands.PingCommand(this));
-        getCommand("vanish").setExecutor(new me.mrlion.lionplugin.commands.VanishCommand(this));
+        Objects.requireNonNull(getCommand("fly")).setExecutor(new me.mrlion.lionplugin.commands.FlyCommand(this));
+        Objects.requireNonNull(getCommand("heal")).setExecutor(new me.mrlion.lionplugin.commands.HealCommand(this));
+        Objects.requireNonNull(getCommand("msg")).setExecutor(new me.mrlion.lionplugin.commands.MsgCommand(this));
+        Objects.requireNonNull(getCommand("lreload")).setExecutor(new me.mrlion.lionplugin.commands.ReloadCommand(this));
+        Objects.requireNonNull(getCommand("lwhitelist")).setExecutor(new me.mrlion.lionplugin.commands.WhitelistCommand(this));
+        Objects.requireNonNull(getCommand("ping")).setExecutor(new me.mrlion.lionplugin.commands.PingCommand(this));
+        Objects.requireNonNull(getCommand("vanish")).setExecutor(new me.mrlion.lionplugin.commands.VanishCommand(this));
 
         getServer().getPluginManager().registerEvents(new me.mrlion.lionplugin.listeners.JoinQuitListener(this), this);
         getServer().getPluginManager().registerEvents(new me.mrlion.lionplugin.listeners.BlockedCommandsListener(this), this);
