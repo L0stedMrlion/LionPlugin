565ce3a0c984aa9dd21f3df0190f46b885a6498e
Mrlion <mrlion4202@gmail.com>
2024-03-15 09:47:55 +0100

    added annotations to build gradle, trying to fix the WhitelistListener

--- src/main/java/me/mrlion/lionplugin/listeners/WhitelistListener.java
+++ src/main/java/me/mrlion/lionplugin/listeners/WhitelistListener.java
@@ -7,27 +7,33 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
 import java.util.Objects;
 
 public class WhitelistListener implements Listener {
 
-    private final FileConfiguration config;
+    private final JavaPlugin plugin;
+    private List<String> whitelistedPlayers;
+    private String kickMessage;
 
     public WhitelistListener(JavaPlugin plugin) {
-        this.config = plugin.getConfig();
+        this.plugin = plugin;
+        this.reloadConfig();
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    public void reloadConfig() {
+        FileConfiguration config = plugin.getConfig();
+        whitelistedPlayers = config.getStringList("whitelist.whitelisted-players");
+        kickMessage = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(config.getString("whitelist.kick-message")));
     }
 
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
         String playerName = event.getPlayer().getName();
-        if (event.getPlayer().isOp()) {
+        if (event.getPlayer().isOp() || whitelistedPlayers.contains(playerName)) {
             return;
         }
-        if (!config.getStringList("whitelist.whitelisted-players").contains(playerName)) {
-            String kickMessage = Objects.requireNonNull(config.getString("whitelist.kick-message"));
-            kickMessage = ChatColor.translateAlternateColorCodes('&', kickMessage);
-            event.disallow(PlayerLoginEvent.Result.KICK_OTHER, kickMessage);
-        }
+        event.disallow(PlayerLoginEvent.Result.KICK_OTHER, kickMessage);
     }
 }
